1d5ac5b8510cef0d43b40c7c4dc53bfbd11f87e9
sentral <jensw@hotmail.com>
2012-05-13 19:23:57 +0200

    Signed-off-by: sentral <jensw@hotmail.com>

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// database section
+$dbhost		=	"";
+$dbuser		=	"";
+$dbpassword	=	"";
+$db			=	"";
+
+$dbmappings = array(
+);
+
+// session settings
+$inactive	=	3000;	// time in seconds of inactivity until session expires
+
+?>
